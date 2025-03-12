5ede32f8be6f18d3e2fef971693f60805653b7d7
estnafinema0 <Kat9765830@gmail.com>
2025-03-12 16:28:24 +0300

    fetcher script updated

--- prompts_fetcher.py
+++ prompts_fetcher.py
@@ -1,11 +1,14 @@
 from collections import deque
+import grequests
 import requests
 import os
 import json
 import pathlib
 from sklearn.metrics import precision_score, recall_score, f1_score
-from dataclasses import dataclass
+from dataclasses import dataclass, asdict
 import pickle
+import itertools
+import multiprocessing
 
 API_URL = "https://llm.ispras.ru/api/chat/completions"
 API_MODEL_URL = "https://llm.ispras.ru/api/models"
@@ -33,6 +36,16 @@ def perform_request(prompt: str, model: str = "llama3.3:latest"):
         print(f"Request failed: {e}")
         return None
 
+def compile_request(prompt: str, model: str = "llama3.3:latest"):
+    payload = {
+        "model": model,
+        "messages": [{"role": "user", "content": prompt}],
+        "format": "json"
+    }
+    # print(payload)
+    return grequests.post(API_URL, headers=headers, json=payload)
+
+
 def calculate_metrics(true_labels, predicted_labels):
     precision = precision_score(true_labels, predicted_labels, average='macro', zero_division=0)
     recall = recall_score(true_labels, predicted_labels, average='macro', zero_division=0)
@@ -47,7 +60,7 @@ class Request:
     left: int = 3
 
 class PromptLoader: 
-    def __init__(self, prompts: list[tuple[str, list[str], list[int]]], model: str = "llama3.3:latest", save_path: str = "Prompts.pickle"):
+    def __init__(self, prompts: list[tuple[str, list[str], list[int]]], model: str = 'mistral-large:123b', save_path: str = "Prompts.pickle"):
         self._prompt_queue = deque(Request(*prompt) for prompt in prompts)
         self.fetched = {}
         self._model = model
@@ -61,11 +74,11 @@ class PromptLoader:
     
     def fetch_next(self):
         prompt = self._prompt_queue.popleft()
-        print(f"{len(self.fetched)}         {prompt}")
         try:
             response = perform_request(prompt.prompt, self._model)
         except KeyboardInterrupt as e:
-            self.save(self._save_path)
+            # self.save(self._save_path)
+            self.save_json()
             raise e
 
         if response is None:
@@ -92,9 +105,95 @@ class PromptLoader:
         while self._prompt_queue:
             self.fetch_next()
             iteration += 1
-            if iteration % 15 == 0:
-                self.save(self._save_path) 
-            
+            if iteration % 200 == 0:
+                # self.save(self._save_path)
+                self.save_json()
+    
+    @staticmethod
+    def fetch_next_async(arg):
+        prompt, model = arg
+        print(prompt.prompt)
+        response = perform_request(prompt.prompt, model)
+
+        if response is None:
+            prompt.left -= 1
+            return (False, prompt) if prompt.left else None
+        
+        predicted_labels = []
+
+        try:
+            predicted_entities = json.loads(response["choices"][0]["message"]["content"])
+            predicted_labels.extend(predicted_entities["ner_tags"])
+        except KeyError:
+            prompt.left -= 1
+            return (False, prompt) if prompt.left else None
+
+        if len(predicted_labels) != len(prompt.correct) or not all(tp == tc for tc, tp, in zip(prompt.tokens, predicted_entities["tokens"])):
+            prompt.left -= 1
+            return (False, prompt) if prompt.left else None
+        return (True, (prompt.prompt, {"correct": prompt.correct, "predicted": predicted_labels, "metric": calculate_metrics(prompt.correct, predicted_labels)}))
+    
+    @staticmethod
+    def fecth_batch(batch, model):
+        rs = (compile_request(prompt.prompt, model) for prompt in batch)
+        return zip(grequests.map(rs), batch)
+
+    @staticmethod
+    def filter_fetched(response, prompt):
+        print(response.status_code)
+        try:
+            response.raise_for_status()
+            response = response.json()
+        except requests.exceptions.RequestException:
+            prompt.left -= 1
+            return (False, prompt) if prompt.left else None
+        
+        if response is None:
+            prompt.left -= 1
+            return (False, prompt) if prompt.left else None
+        
+        predicted_labels = []
+
+        try:
+            predicted_entities = json.loads(response["choices"][0]["message"]["content"])
+            predicted_labels.extend(predicted_entities["ner_tags"])
+        except KeyError:
+            prompt.left -= 1
+            return (False, prompt) if prompt.left else None
+
+        if len(predicted_labels) != len(prompt.correct) or not all(tp == tc for tc, tp, in zip(prompt.tokens, predicted_entities["tokens"])):
+            prompt.left -= 1
+            return (False, prompt) if prompt.left else None
+        return (True, (prompt.prompt, {"correct": prompt.correct, "predicted": predicted_labels, "metric": calculate_metrics(prompt.correct, predicted_labels)}))
+
+    def fetch_all_async(self):
+        batch_size = 1
+        while self._prompt_queue:
+            batch = list(itertools.islice(self._prompt_queue, batch_size))
+            result = []
+            try:
+                for _ in range(len(batch)):
+                    self._prompt_queue.popleft()
+                fetched = PromptLoader.fecth_batch(batch, self._model)
+                result.extend(PromptLoader.filter_fetched(fetch, prompt) for fetch, prompt in fetched)
+                # with multiprocessing.Pool(8) as p:
+                #     result = p.map(PromptLoader.fetch_next_async, zip(batch, [self._model] * batch_size))
+            except KeyboardInterrupt as e:
+                print("AAAAAAA")
+                for v in reversed(batch):
+                    self._prompt_queue.appendleft(v)
+                self.save_json()
+                print("DIE")
+                raise e
+            for succ, r in filter(lambda x: x is not None, result):
+                if succ:
+                    self.fetched[r[0]] = r[1]
+                else:
+                    self._prompt_queue.append(r)
+            self.save_json()
+            print(len(self.fetched))
+
+
     def save(self, path: pathlib.Path):
         name, ext = os.path.splitext(path)
         pathlib.Path("backup").mkdir(exist_ok=True)
@@ -108,6 +207,22 @@ class PromptLoader:
         
         with open(f"{name}-latest{ext}", "wb") as file:
             pickle.dump(self, file, protocol=pickle.HIGHEST_PROTOCOL)
+    
+    def save_json(self):
+        pathlib.Path("backup").mkdir(exist_ok=True)
+        try:
+            with open(f"prompts-latest.json", 'r') as prev, open(f"backup/prompts-{self._count}.json", 'w') as copy:
+                copy.write(prev.read())
+            with open(f"left-requests-latest.json", 'r') as prev, open(f"backup/left-requests-{self._count}.json", 'w') as copy:
+                copy.write(prev.read())
+            self._count += 1
+        except FileNotFoundError:
+            pass
+
+        with open('prompts-latest.json', 'w') as file, open('left-requests-latest.json', 'w') as f:
+            json.dump(self.fetched, file)
+            data = list(map(asdict, self._prompt_queue))
+            json.dump(data, f)
 
     @staticmethod
     def load(path: os.PathLike = "Prompts-latest.pickle") -> "PromptLoader":
@@ -115,15 +230,28 @@ class PromptLoader:
         with open(path, "rb") as file:
             loaded_file = pickle.load(file)
         return loaded_file
+    
+    @staticmethod
+    def from_json(fetched, left, count):
+        loader = PromptLoader(())
+        loader._prompt_queue = deque(Request(**l) for l in left)
+        loader.fetched = fetched
+        loader._count = count
+        return loader
+
 
 def main():
     if False:
-        fetcher = PromptLoader(prompts, 'mistral-large:123b')
+        fetcher = PromptLoader(prompts, 'llama3.1:405b')
     else:
-        fetcher = PromptLoader.load()
+        with open('prompts-latest.json') as prompts_json, open('left-requests-latest.json') as left_json:
+            prompts_loaded = json.load(prompts_json)
+            left_loaded = json.load(left_json)
+            new_loader = PromptLoader.from_json(prompts_loaded, left_loaded, 609)
     
-    fetcher.fetch_all()
+    new_loader.fetch_all_async()
 
 
 if __name__ == "__main__":
     main()
+
